958b44954ce893747c4d7454aa041d1346b27891
Ulfatjon <frontenduchun@gmail.com>
2025-07-08 14:11:58 +0500

    The project is finished.

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -104,8 +104,7 @@ export default function Users() {
     const year = date.getFullYear();
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
-    return `${day}.${month}.${year} - ${hours}:${minutes}:${seconds}`;
+    return `${day}.${month}.${year} - ${hours}:${minutes}`;
   }
 
   return (
